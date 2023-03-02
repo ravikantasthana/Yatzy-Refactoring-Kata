470c5fb829777ac925336b9c778c233baf44776c
raviasthana <ravi.asthana@gmail.com>
2023-03-02 20:17:30 +0000

    yatzy with allMatch -> iterator.next

--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -6,17 +6,15 @@ public class YatzyTest {
     @Test
     public void chance_scores_sum_of_all_dice() {
         int expected = 15;
-        int actual = Yatzy.chance(2,3,4,5,1);
+        int actual = Yatzy.chance(new DiceRolls(2, 3, 4, 5, 1));
         assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3,3,4,5,1));
+        assertEquals(16, Yatzy.chance(new DiceRolls(3, 3, 4, 5, 1)));
     }
 
     @Test public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
+        assertEquals(50, Yatzy.yatzy(new DiceRolls(4,4,4,4,4)));
+        assertEquals(50, Yatzy.yatzy(new DiceRolls(6,6,6,6,6)));
+        assertEquals(0, Yatzy.yatzy(new DiceRolls(6,6,6,6,3)));
     }
 
     @Test public void test_1s() {
